044b886d73939b78bde4524a36ee9869df25c7fe
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-01 15:04:35 +0600

    update code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
 // common function for empty input field 
 const emptyInput = id => {document.getElementById(id).value = '';}
 
+// Common Function for empty card-container
+const emptyCardContainer = id => {
+  const getCardContainer = document.getElementById(id);
+  getCardContainer.textContent = '';
+}
+
+// Common function for add ClassList
+const addClass = err => {
+  const getErr = document.getElementById(err);
+  getErr.classList.add('d-none');
+}
+
+// Common function for remove ClassList
+const removeClass = err => {
+  const getErr = document.getElementById(err);
+  getErr.classList.remove('d-none');
+}
+
+
+/////////////////// main work ///////////////////
 const searchBtn = () => {
   const searchField = document.getElementById('search-field');
   const searchText = searchField.value;
@@ -11,51 +31,48 @@ const searchBtn = () => {
   .then(res => res.json())
   .then(data => {
     if(searchField.value === ''){
-      const getErr = document.getElementById('err');
-      getErr.classList.remove('d-none');
-      const getErrMsg = document.getElementById('err-msg');
-      getErrMsg.innerText = 'Search Any Phone';
+      removeClass('err')
       emptyInput('search-field');
+      const getErrMsg = document.getElementById('err-msg');
+      getErrMsg.innerText = '';
     }
     else if(data.data.length === 0){
-      const getErr = document.getElementById('err');
-      getErr.classList.remove('d-none');
+      removeClass('err')
+      emptyInput('search-field');
+      emptyCardContainer('card-container');
       const getErrMsg = document.getElementById('err-msg');
       getErrMsg.innerText = 'No data found';
-      emptyInput('search-field');
     }
     else{
       displaySearch(data.data)
-      const getErr = document.getElementById('err');
-      getErr.classList.add('d-none');
+      addClass('err')
+      emptyCardContainer('card-details')
     }
   })
 }
 
-
-
 const displaySearch = (getData) => {
-  // condition  
-  console.log(getData);
+  // console.log(getData.length);
   const getCardContainer = document.getElementById('card-container');
-  // empty previous result 
   getCardContainer.textContent = '';
-  getData.forEach(info => {
-    // console.log(info);
-  const div = document.createElement('div');
-  div.classList.add('col-lg-4', 'col-12', 'mb-4');
-  div.innerHTML = `
-    <div class="card" style="width: 18rem;">
-      <img src="${info.image}" class="card-img-top" alt="...">
+  // Slice Card container
+  const totalResult = getData.slice(0,20);
+  // console.log(totalResult.length);
+  totalResult.forEach(getSlice => {
+    const div = document.createElement('div');
+    div.classList.add('col-lg-4', 'col-md-6', 'col-sm-12', 'col-12', 'mb-4');
+    div.innerHTML = `
+    <div class="card text-center pt-5">
+      <img src="${getSlice.image}" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Name: ${info.phone_name}</h5>
-        <h5 class="card-title">Brand: ${info.brand}</h5>
-        <button onclick="detailsBtn('${info.slug}')" class="btn btn-primary">See Details</button>
+        <h5 class="card-title">Name: ${getSlice.phone_name}</h5>
+        <h5 class="card-title">Brand: ${getSlice.brand}</h5>
+        <button onclick="detailsBtn('${getSlice.slug}')" class="btn btn-primary">See Details</button>
       </div>
     </div>
   `;
   getCardContainer.appendChild(div);
-  emptyInput('search-field');
+  emptyInput('search-field')
   })
 }
 
@@ -70,11 +87,10 @@ const detailsBtn = details => {
 const phoneDetails = getPhoneInfo => {
   console.log(getPhoneInfo.name);
   const cardDetail = document.getElementById('card-details');
-  // empty card 
   cardDetail.textContent = '';
-  // others
-  const myObject = `${getPhoneInfo.others}`
-  console.log(myObject);
+  // distructuring 
+  const  {GPS,WLAN,NFC,Radio,USB} = getPhoneInfo.others;
+  const  {name,releaseDate,mainFeatures} = getPhoneInfo
 
   // get sensors 
   const arr = `${getPhoneInfo.mainFeatures.sensors}`;
@@ -83,35 +99,28 @@ const phoneDetails = getPhoneInfo => {
   const div = document.createElement('div');
   div.classList.add('col-12', 'd-flex', 'justify-content-center');
   div.innerHTML = `
-    <div class="card custom-card" style="width: 40rem;">
+    <div class="card custom-card pt-5" style="width: 40rem;">
       <img src="${getPhoneInfo.image}" class="card-img-top" alt="...">
       <div class="card-body">
-      <p class="card-title"><span class="fw-bold">Name: </span> ${getPhoneInfo.name}</p>
-        <p class="card-title"><span class="fw-bold">Release: </span> ${getPhoneInfo.releaseDate}</p>
+      <p class="card-title"><span class="fw-bold">Name: </span> ${name}</p>
+        <p class="card-title"><span class="fw-bold">Release: </span> ${releaseDate ? getPhoneInfo.releaseDate : 'No release date found' }</p>
         <p class="card-title"> <span class="fw-bold">Sensors: </span> ${sensors}</p>
         <p class="card-title"> <span class="fw-bold">Main Feature: 
-        </span> <br> <span class="text-success fw-bold">Storage:</span>  ${getPhoneInfo.mainFeatures.storage}
-        <br> <span class="text-success fw-bold">DisplaySize:</span> ${getPhoneInfo.mainFeatures.displaySize}
-        <br> <span class="text-success fw-bold">Chipset:</span> ${getPhoneInfo.mainFeatures.chipSet}
-        <br> <span class="text-success fw-bold">Memory:</span> ${getPhoneInfo.mainFeatures.memory}
+        </span> <br> <span class="text-success fw-bold">Storage:</span>  ${mainFeatures.storage}
+        <br> <span class="text-success fw-bold">DisplaySize:</span> ${mainFeatures.displaySize}
+        <br> <span class="text-success fw-bold">Chipset:</span> ${mainFeatures.chipSet}
+        <br> <span class="text-success fw-bold">Memory:</span> ${mainFeatures.memory}
         </p>
         <p class="card-title"><span class="fw-bold">Other Feature: </span> <br> 
-        <span class="text-success fw-bold">WLAN:</span> ${getPhoneInfo.others.WLAN} <br>
-        <span class="text-success fw-bold">Bluetooth:</span> ${getPhoneInfo.others.Bluetooth} <br>
-        <span class="text-success fw-bold">GPS:</span> ${getPhoneInfo.others.GPS} <br>
-        <span class="text-success fw-bold">NFC:</span> ${getPhoneInfo.others.NFC} <br>
-        <span class="text-success fw-bold">Radio:</span> ${getPhoneInfo.others.Radio} <br>
-        <span class="text-success fw-bold">USB:</span> ${getPhoneInfo.others.USB}
+        <span class="text-success fw-bold">WLAN:</span> ${WLAN} <br>
+        <span class="text-success fw-bold">Bluetooth:</span> ${Bluetooth} <br>
+        <span class="text-success fw-bold">GPS:</span> ${GPS} <br>
+        <span class="text-success fw-bold">NFC:</span> ${NFC} <br>
+        <span class="text-success fw-bold">Radio:</span> ${Radio} <br>
+        <span class="text-success fw-bold">USB:</span> ${USB}
         </p>
       </div>
     </div>
   `;
   cardDetail.appendChild(div);
 }
-
-
-
-
-// for(const phoneDetail in getPhoneInfo){
-//   console.log(`${phoneDetail}: ${getPhoneInfo[phoneDetail]}`);
-// }
